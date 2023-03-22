8bb2e950364d281938b724a6dfd6f7b0938cff4a
Ana Mateska <mateskaana@yahoo.com>
2023-03-22 20:16:01 +0100

    Project created

---
+++ src/main/java/com/example/embedsocialfilterforreviews/EmbedSocialFilterForReviewsApplication.java
@@ -0,0 +1,13 @@
+package com.example.embedsocialfilterforreviews;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmbedSocialFilterForReviewsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EmbedSocialFilterForReviewsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/embedsocialfilterforreviews/EmbedSocialFilterForReviewsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.embedsocialfilterforreviews;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmbedSocialFilterForReviewsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
